7d81b84816718e816dd13bda058fa79d19091a86
Fabio Gorodscy <fabiogo@outlook.com>
2023-06-28 14:25:31 -0300

    added movies page

---
+++ src/Filter.js
@@ -0,0 +1,44 @@
+import Col from 'react-bootstrap/Col'
+import ListGroup from 'react-bootstrap/ListGroup'
+import {fetchGenres} from './util/Api.js'
+import { useEffect, useState } from "react"
+
+function Filter({clickHandler}) {
+  let [genres, setGenres] = useState([])
+  useEffect(() => {
+    (async ()=>{
+      const results = await fetchGenres()
+      setGenres(results)
+    })()
+  }, [])
+
+  function listClick(e){
+    if(e.target.children.length){
+      e.target.children[0].click()
+    }
+  }
+
+  return (
+    <Col xs="2">
+      <ListGroup as="ul">
+        <ListGroup.Item as="li" active>
+          Genres
+        </ListGroup.Item>
+        {genres.length?genres.map((genre,i)=>(
+          <ListGroup.Item key={i} as="li" onClick={listClick}>
+            <input 
+              id={genre.name} 
+              type="checkbox" 
+              value={genre.id} 
+              position={i} 
+              onChange={clickHandler}
+              className="me-2"/> 
+            <label htmlFor={genre.name}>{genre.name}</label>
+          </ListGroup.Item>
+        )):<p>Loading...</p>}
+      </ListGroup>
+    </Col>
+  );
+}
+
+export default Filter;
---
+++ src/Movie.js
@@ -0,0 +1,38 @@
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import Ratio from 'react-bootstrap/Ratio'
+import Image from 'react-bootstrap/Image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faImage } from "@fortawesome/free-solid-svg-icons";
+
+function Movie({movie}) {
+  return (
+    <Col xs="3" className="mb-3">
+      <Card>
+        <Card.Body className="p-0">
+          <Row>
+            <Col xs="6" style={{height: "25vh"}}>
+              {movie.poster_path?(
+                <Image className="h-100" fluid rounded src={`https://image.tmdb.org/t/p/w220_and_h330_face/${movie.poster_path}`}></Image>
+                  ):(
+                    <div className="text-center fs-1"><FontAwesomeIcon className="position-absolute top-50 translate-middle" icon={faImage} /></div>
+                  )}
+            </Col>
+            <Col xs="6">
+              <Card.Text className="h-100 m-2"><strong className="h-50 overflow-y-hidden">{movie.title}</strong>
+              </Card.Text>
+              <Card.Text className="position-absolute bottom-0 m-2">
+                Rating: {Math.round(movie.vote_average*10)}%<br/>
+                {movie.release_date}<br/>
+                <Card.Link href="#">Read more...</Card.Link>
+              </Card.Text>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+}
+
+export default Movie;
